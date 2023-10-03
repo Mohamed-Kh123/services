37e50173e7e4beefbd749a2172e3671472285b66
Mohammed.Khrais <mohammed.khrais@nawagrow.com>
2023-10-03 21:28:24 +0300

    ....

--- public/js/admin/Resources_js_pages_category_Create_vue.js
+++ public/js/admin/Resources_js_pages_category_Create_vue.js
@@ -13,34 +13,35 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 var form = {
   config: {
-    resource: "company"
+    resource: "category"
   },
   inputs: [{
     component: "input",
     model: "name",
     label: "name",
-    cols: 6,
+    cols: 12,
     rules: {
       required: true
     },
     multiLang: true
   }, {
-    component: "input",
-    model: "commission",
-    label: "commission",
-    cols: 6,
+    component: "editor",
+    model: "description",
+    label: "description",
+    cols: 12,
     rules: {
       required: true
     },
     multiLang: true
+  }, {
+    component: "image",
+    model: "image",
+    label: "image",
+    image_url_option: "image_url",
+    cols: 6
   }, {
     component: "switch",
     model: "is_active",
-    cols: 6,
-    label: "is_active"
-  }, {
-    component: "switch",
-    model: "has_commission",
     cols: 6,
     label: "is_active"
   }]
