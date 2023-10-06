7151feae46d9db6360d75349b5da4568df59dcc0
BaraaEraif2000 <baraaeraif@gmail.com>
2023-10-06 22:03:53 +0300

    edits

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('optimize', function () {
+    \Illuminate\Support\Facades\Artisan::call('optimize:clear');
+    dd('optimize');
 });
